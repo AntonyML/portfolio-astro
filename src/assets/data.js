1b84ed0a8193ee47c3c875b18210a4236601146f
ANTONY JAFETH MONGE LOPEZ <antony.mongelopez@ucr.ac.cr>
2025-06-06 22:50:07 -0600

    Primer commit con la mayoria del porfolio, se agrego el home, about y los projests

---
+++ src/assets/data.js
@@ -0,0 +1,123 @@
+export const socials = [{ logo: 'bx bxl-youtube' }, { logo: 'bx bxl-linkedin-square' }, { logo: 'bx bxl-instagram' }, { logo: 'bx bxl-facebook-circle' }, { logo: 'bx bxl-github' }];
+
+export const info = [{ text: 'About' }, { text: 'Projects' }];
+
+export const technologies = [
+  // Lenguajes de programación
+  { text: 'JavaScript', logo: 'bx bxl-javascript' },
+  { text: 'TypeScript', logo: 'bx bxl-typescript' },
+  { text: 'Python', logo: 'bx bxl-python' },
+  { text: 'Kotlin', logo: 'https://cdn.simpleicons.org/kotlin' },
+  { text: 'PHP', logo: 'bx bxl-php' },
+  { text: 'C', logo: 'https://cdn.simpleicons.org/c' },
+  { text: 'C++', logo: 'https://cdn.simpleicons.org/cplusplus' },
+  { text: 'C#', logo: 'https://cdn.worldvectorlogo.com/logos/c--4.svg' },
+  { text: 'Java', logo: 'bx bxl-java' },
+  { text: 'Shell', logo: 'https://cdn.simpleicons.org/gnubash' },
+
+  // Lenguajes de marcado / estilos
+  { text: 'HTML', logo: 'bx bxl-html5' },
+  { text: 'CSS', logo: 'bx bxl-css3' },
+
+  // Frameworks / Librerías Frontend
+  { text: 'React', logo: 'bx bxl-react' },
+  { text: 'Vue', logo: 'bx bxl-vuejs' },
+  { text: 'Angular', logo: 'bx bxl-angular' },
+  { text: 'Svelte', logo: 'https://cdn.simpleicons.org/svelte' },
+  { text: 'Next.js', logo: 'https://cdn.simpleicons.org/nextdotjs' },
+  { text: 'Qwik', logo: 'https://cdn.simpleicons.org/qwik' },
+  { text: 'Astro', logo: 'https://cdn.simpleicons.org/astro' },
+  { text: 'Flutter', logo: 'https://cdn.simpleicons.org/flutter' },
+  { text: 'Android', logo: 'bx bxl-android' },
+
+  // Frameworks / Librerías Backend
+  { text: 'Node.js', logo: 'bx bxl-nodejs' },
+  { text: 'Laravel', logo: 'https://cdn.simpleicons.org/laravel' },
+  { text: 'Symfony', logo: 'https://cdn.simpleicons.org/symfony' },
+  { text: 'Django', logo: 'https://cdn.simpleicons.org/django' },
+  { text: 'Flask', logo: 'https://cdn.simpleicons.org/flask' },
+  { text: 'FastAPI', logo: 'https://cdn.simpleicons.org/fastapi' },
+  { text: 'NestJS', logo: 'https://cdn.simpleicons.org/nestjs' },
+   { text: 'Astro', logo: 'https://cdn.simpleicons.org/astro' },
+
+  // Bases de datos
+  { text: 'PostgreSQL', logo: 'bx bxl-postgresql' },
+  { text: 'MySQL', logo: 'https://cdn.simpleicons.org/mysql' },
+  { text: 'SQL Server', logo: 'https://static.cdnlogo.com/logos/m/6/microsoft-sql-server.svg' },
+  { text: 'Oracle', logo: 'https://cdn.worldvectorlogo.com/logos/oracle-6.svg' },
+
+  // Herramientas / Entornos / Plataformas
+  { text: 'Git', logo: 'bx bxl-git' },
+  { text: 'GitHub', logo: 'bx bxl-github' },
+];
+
+
+
+
+export const projects = [
+   {
+      title: 'Blog',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-blog',
+      color: '#f3c201',
+   },
+   {
+      title: 'Chatgpt API',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. .',
+      image: 'image-chatgpt-api',
+      color: '#1629ab',
+   },
+   {
+      title: 'Ecommerce',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-ecommerce',
+      color: '#045fb7',
+   },
+   {
+      title: 'Facebook Clone',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-facebook-clone',
+      color: '#146866',
+   },
+   {
+      title: 'Instagram Clone',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-instagram-clone',
+      color: '#17193f',
+   },
+   {
+      title: 'Slider',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-slider',
+      color: '#bbbbbb',
+   },
+   {
+      title: 'Spotify Clone',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-spotify-clone',
+      color: '#49d53c',
+   },
+   {
+      title: 'Youtube Clone',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-youtube-clone',
+      color: '#f7030c',
+   },
+];
+
+export const buttons = [
+   {
+      github: 'bx bxl-github',
+      demo: 'bx bx-desktop',
+      back: 'bx bxs-left-arrow-circle',
+      hrefgithub: 'https://github.com/MilaDev-JS'
+   },
+];
